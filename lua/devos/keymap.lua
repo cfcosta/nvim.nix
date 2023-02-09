775e51d84cb3fc1a8122993a53e8ddccab1a07e2
Cainã Costa <me@cfcosta.com>
2023-02-09 08:01:17 -0300

    feat: add keybinding to open current dir

--- lua/devos/keymap.lua
+++ lua/devos/keymap.lua
@@ -1,3 +1,4 @@
 vim.keymap.set('n', '<space>wv', ':vsp<cr>', opts)
 vim.keymap.set('n', '<space>ws', ':sp<cr>', opts)
 vim.keymap.set('n', '<space>wc', ':close<cr>', opts)
+vim.keymap.set('n', '<space>o-', ':edit %:p:h', opts)
